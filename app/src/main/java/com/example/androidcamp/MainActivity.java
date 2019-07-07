35544a4b7425f7b52e6026ce0beea512fe4b2de6
Black-Garlic <hn1226@naver.com>
2019-07-07 21:43:08 +0900

    3.3 15 : 10

--- app/src/main/java/com/example/androidcamp/MainActivity.java
+++ app/src/main/java/com/example/androidcamp/MainActivity.java
@@ -1,119 +1,50 @@
 package com.example.androidcamp;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.ShareCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "CalculatorActivity";
 
-    private Calculator mCalculator;
-
-    private EditText mOperandOneEditText;
-    private EditText mOperandTwoEditText;
-
-    private TextView mResultTextView;
+    private TextView mHelloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the calculator class and all the views
-        mCalculator = new Calculator();
-        mResultTextView = findViewById(R.id.operation_result_text_view);
-        mOperandOneEditText = findViewById(R.id.operand_one_edit_text);
-        mOperandTwoEditText = findViewById(R.id.operand_two_edit_text);
-    }
+        mHelloTextView = findViewById(R.id.hello_textview);
 
-    /**
-     * OnClick method called when the add Button is pressed.
-     */
-    public void onAdd(View view) {
-        compute(Calculator.Operator.ADD);
-    }
-
-    /**
-     * OnClick method called when the subtract Button is pressed.
-     */
-    public void onSub(View view) {
-        compute(Calculator.Operator.SUB);
-    }
-
-    /**
-     * OnClick method called when the divide Button is pressed.
-     */
-    public void onDiv(View view) {
-        try {
-            compute(Calculator.Operator.DIV);
-        } catch (IllegalArgumentException iae) {
-            Log.e(TAG, "IllegalArgumentException", iae);
-            mResultTextView.setText(getString(R.string.computationError));
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
         }
     }
 
-    /**
-     * OnClick method called when the multiply Button is pressed.
-     */
-    public void onMul(View view) {
-        compute(Calculator.Operator.MUL);
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
     }
 
-    private void compute(Calculator.Operator operator) {
-        double operandOne;
-        double operandTwo;
-        try {
-            operandOne = getOperand(mOperandOneEditText);
-            operandTwo = getOperand(mOperandTwoEditText);
-        } catch (NumberFormatException nfe) {
-            Log.e(TAG, "NumberFormatException", nfe);
-            mResultTextView.setText(getString(R.string.computationError));
-            return;
-        }
+    public void changeColor(View view) {
+        Random random = new Random();
 
-        String result;
-        switch (operator) {
-            case ADD:
-                result = String.valueOf(
-                        mCalculator.add(operandOne, operandTwo));
-                break;
-            case SUB:
-                result = String.valueOf(
-                        mCalculator.sub(operandOne, operandTwo));
-                break;
-            case DIV:
-                result = String.valueOf(
-                        mCalculator.div(operandOne, operandTwo));
-                break;
-            case MUL:
-                result = String.valueOf(
-                        mCalculator.mul(operandOne, operandTwo));
-                break;
-            default:
-                result = getString(R.string.computationError);
-                break;
-        }
-        mResultTextView.setText(result);
-    }
+        String colorName = mColorArray[random.nextInt(20)];
 
-    /**
-     * @return the operand value entered in an EditText as double.
-     */
-    private static Double getOperand(EditText operandEditText) {
-        String operandText = getOperandText(operandEditText);
-        return Double.valueOf(operandText);
-    }
+        int colorResourceName = getResources().getIdentifier(colorName,
+                "color", getApplicationContext().getPackageName());
 
-    /**
-     * @return the operand text which was entered in an EditText.
-     */
-    private static String getOperandText(EditText operandEditText) {
-        return operandEditText.getText().toString();
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+
+        mHelloTextView.setTextColor(colorRes);
     }
 }
